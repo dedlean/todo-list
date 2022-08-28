d7b8f485ecd13de52a253b8d8a3941ef0bda7139
dedlean <ghostergram@gmail.com>
2022-08-28 19:20:18 +0000

    add main features

---
+++ main.js
@@ -0,0 +1,89 @@
+const tasksNode = document.querySelector('.js-tasks');
+const inputNode = document.querySelector('.js-add-input');
+const btnNode = document.querySelector('.js-add-btn');
+
+let tasks = [];
+
+//---------------------------------------------------------
+
+function addTask(text) {
+	const task = {
+		text,
+		done: false,
+		id: `${ Math.random().toFixed(3) * 1e3 }`
+	};
+
+	tasks.push(task);
+}
+
+function deleteTask(id) {
+	for( let i = 0; i < tasks.length; i++ ) {
+		if( tasks[i].id == id ) tasks.splice(i, 1);
+	}
+}
+
+function doTask(id) {
+	for( let i = 0; i < tasks.length; i++ ) {
+		if( tasks[i].id == id ) {
+			if( tasks[i].done == false ) {
+				tasks[i].done = true;
+				return;
+			}
+			if( tasks[i].done == true )	{
+				tasks[i].done = false;
+				return;
+			}
+		}
+	}
+}
+
+//---------------------------------------------------------
+
+function render() {
+	console.log(tasks);
+	let html = '';
+
+	tasks.forEach(task => {
+		if(task.done) {
+			html += `
+			<div id="${task.id}">
+				<button data-id="${task.id}">Done</button>
+				<button data-id="${task.id}">Delete</button>
+				<s>${task.text}</s>
+			</div>
+		`;
+		return;
+		}
+
+		html += `
+			<div id="${task.id}">
+				<button data-id="${task.id}">Done</button>
+				<button data-id="${task.id}">Delete</button>
+				${task.text}
+			</div>
+		`;
+	})
+
+	tasksNode.innerHTML = html;
+}
+
+btnNode.addEventListener('click', () => {
+	const text = inputNode.value;
+	addTask(text);
+	inputNode.value = '';
+	render();
+});
+
+tasksNode.addEventListener('click', (event) => {
+	if(event.target.innerHTML == 'Done') {
+		const id = event.target.getAttribute('data-id');
+		doTask(id);
+		render();
+	}
+
+	if(event.target.innerHTML == 'Delete') {
+		const id = event.target.getAttribute('data-id');
+		deleteTask(id);
+		render();
+	}
+})
